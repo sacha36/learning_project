cd70b9f01c071718ee174bb7594ac6262c1947f9
Sacha <sacha.lm.36@gmail.com>
2015-06-20 19:46:21 +0200

    new file

---
+++ gestion_exception.py
@@ -0,0 +1,15 @@
+#! etc/bin
+
+
+## hello  ici on va apprendre a gerer les msg d'erreur
+
+
+
+
+
+
+print("hello on va tester differents types derreur")
+print(" le premier type div/0")
+print(" le second les var non def ")
+print(" et ensuite l'incompatibilité des types ") 
+
